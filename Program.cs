124cefa3a9724ab4ae8e64f44be9fc405d54d61b
cassialima1329 <cahsantos699805@gmail.com>
2025-10-17 21:18:02 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿string senhaDigitada;
+string senhaCorreta = "123abc";
+
+Console.Write("Digite sua senha...: ");
+senhaDigitada = Console.ReadLine()!;
+
+bool acertou = senhaDigitada == senhaCorreta;
+
+if (acertou)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Senha correta!!!");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("ERROOOOOOOU!!!");
+}
+
+Console.ResetColor();
